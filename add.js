5caf7baab3fe5648b81ce5f496dd7dedcca27920
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-05-07 15:53:53 +0300

    mohammad

---
+++ add.js
@@ -0,0 +1,38 @@
+const filterNumbers = (numbers) => {
+  return numbers
+    .split("+")
+    .map((ele) => parseInt(ele))
+    .filter((ele) => ele <= 1000);
+};
+
+const add = (numbers) => {
+  if (numbers === undefined || numbers === "") {
+    return 0;
+  }
+
+  const numFilteredArrayAllowed = filterNumbers(numbers);
+
+  const negativeNumbersArray = numFilteredArrayAllowed.filter((num) => num < 0);
+  if (negativeNumbersArray.length > 0) {
+    throw new Error(
+      `Negatives are not allowed: (${negativeNumbersArray.join(" | ")})`
+    );
+  }
+
+  return numFilteredArrayAllowed.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+};
+
+const addMultiple = (numbers) => {
+  const numFilteredArrayAllowed = filterNumbers(numbers);
+
+  if (numFilteredArrayAllowed.length > 2) {
+    throw new Error("Support up to 2 numbers only");
+  }
+
+  return add(numbers);
+};
+
+module.exports = { add, addMultiple };
